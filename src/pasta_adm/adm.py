6ba9c5e751d1e5123630a9f5c3bbbb06c5a5a519
NOT-SUP-LEANDRO <NOT-SUP-LEANDRO@REN-SUP-LEANDRO>
2024-11-05 16:48:27 -0300

    Validação!

--- src/pasta_adm/adm.py
+++ src/pasta_adm/adm.py
@@ -1130,7 +1130,6 @@ def inserirCliente():
 @admin.route('/adm/clientes', methods=['GET','POST'])
 def cadastro():
     formulario = request.form
-    id = formulario['id_cli']
     codigo = formulario['codigo_cli']
     nome = formulario['nome_cli']
     
@@ -1138,7 +1137,8 @@ def cadastro():
         codigodb=Cursor.execute("SELECT codigo_cli FROM clientes WHERE codigo_cli= %s",(codigo,))
         if codigodb:
             flash('Código de Cliente já registrado')
-        Cursor.execute("INSERT INTO Clientes (codigo_cli, nome_cli) VALUES(%s, %s, %s, %s)",(codigo, nome))
+        Cursor.execute("INSERT INTO Clientes (codigo_cli, nome_cli) VALUES(%s, %s)",(codigo, nome))
         mysql.commit()
+        Cursor.close()
         
     return redirect (url_for('/adm'))
--- src/pasta_clientes/clientes.py
+++ src/pasta_clientes/clientes.py
@@ -14,7 +14,6 @@ def inserirCliente():
 @clientes.route('/clientes', methods=['GET','POST'])
 def cadastro ():
     formulario = request.form
-    id = formulario['id_cli']
     codigo = formulario['codigo_cli']
     nome = formulario['nome_cli']
     
@@ -22,7 +21,8 @@ def cadastro ():
         codigodb=Cursor.execute("SELECT codigo_cli FROM clientes WHERE codigo_cli= %s",(codigo,))
         if codigodb:
             flash('Código de Cliente já registrado')
-        Cursor.execute("INSERT INTO Clientes (codigo_cli, nome_cli) VALUES(%s, %s, %s, %s)",(codigo, nome))
+        Cursor.execute("INSERT INTO Clientes (codigo_cli, nome_cli) VALUES(%s, %s)",(codigo, nome))
         mysql.commit()
+        Cursor.close()
         
     return redirect (url_for('/adm'))
